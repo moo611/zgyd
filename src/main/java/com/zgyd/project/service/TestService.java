4859b345d289a4cc45e4c160b4ac47e953da8a5a
moo611 <lnpjjds@gmail.com>
2023-09-07 14:33:58 +0800

    first commit

---
+++ src/main/java/com/zgyd/project/service/TestService.java
@@ -0,0 +1,17 @@
+package com.zgyd.project.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+
+
+
+    public String test(){
+
+        return "Hello World";
+    }
+
+}
